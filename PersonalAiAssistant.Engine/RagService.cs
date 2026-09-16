4269f699ec4f9ce04c5662251ec7f4217103adc9
mahdi <mmk90@yahoo.com>
2026-09-16 20:29:32 +0330

    Add Rag

---
+++ PersonalAiAssistant.Engine/RagService.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PersonalAiAssistant.Contracts.Interfaces;
+using PersonalAiAssistant.Contracts.Models;
+using PersonalAiAssistant.Infrastructure.Services.VectorDb;
+using System.Text;
+
+namespace PersonalAiAssistant.Engine
+{
+    public class RagService
+    {
+        private static string prompt = null;
+            
+        private readonly IEmbeddingClient _embeddingClient;
+        private readonly QdrantService _qdrantService;
+        private readonly ConversationService _conversationService;
+        private readonly IPromptBuilder _promptBuilder;
+        private readonly IChatClient _chatClient;
+
+        public RagService(
+            IEmbeddingClient embeddingClient,
+            QdrantService qdrantService,
+            ConversationService conversationService,
+            [FromKeyedServices("RagPromptBuilder")] IPromptBuilder promptBuilder,
+            IChatClient chatClient)
+        {
+            _embeddingClient = embeddingClient;
+            _qdrantService = qdrantService;
+            _conversationService = conversationService;
+            _promptBuilder = promptBuilder;
+            _chatClient = chatClient;
+        }
+
+        public async Task<ChatResponse> AskAsync(string question)
+        {
+            //-------------------------------------------------
+            // Embedding
+            //-------------------------------------------------
+
+            var embedding =
+                await _embeddingClient.CreateEmbeddingAsync(question);
+
+            //-------------------------------------------------
+            // Vector Search
+            //-------------------------------------------------
+
+            var documents =
+                await _qdrantService.SearchAsync(embedding);
+
+            if (documents.Count == 0)
+                return null;
+
+            //------------------------------------------
+            // Build Prompt
+            //------------------------------------------
+            if (prompt == null)
+                prompt =
+                   await _promptBuilder.BuildAsync();
+
+            var builder = new StringBuilder(prompt);
+
+            for (var i = 0; i < documents.Count; i++)
+            {
+                builder.AppendLine(
+                    $"========== Document {i + 1} ==========");
+
+                builder.AppendLine();
+
+                builder.AppendLine(documents[i].Text);
+
+                builder.AppendLine();
+            }
+
+            var finalPrompt = builder.ToString();
+
+            //-------------------------------------------------
+            // Conversation
+            //-------------------------------------------------
+
+            _conversationService.AddSystemMessage(finalPrompt);
+            _conversationService.AddUserMessage(question);
+
+            //-------------------------------------------------
+            // LLM
+            //-------------------------------------------------
+
+            var answer =
+                await _chatClient.ChatAsync(_conversationService.Conversation);
+
+            _conversationService.AddAssistantMessage(answer);
+
+            //-------------------------------------------------
+            // Response
+            //-------------------------------------------------
+
+            var response = new ChatResponse
+            {
+                Answer = answer
+            };
+
+            foreach (var source in documents)
+            {
+                var item =
+                    $"{source.FileName} (Chunk {source.ChunkNumber})";
+
+                if (!response.Sources.Contains(item))
+                {
+                    response.Sources.Add(item);
+                }
+            }
+
+            return response;
+        }
+    }
+}
--- PersonalAiAssistant/Controllers/ChatController.cs
+++ PersonalAiAssistant/Controllers/ChatController.cs
@@ -9,13 +9,16 @@ namespace PersonalAiAssistant.Controllers;
 [Route("api/chat")]
 public class ChatController : ControllerBase
 {
+    private readonly RagService _ragService;
     private readonly AssistantService _assistantService;
     private readonly ConversationService _conversationService;
 
     public ChatController(
+        RagService ragService,
         AssistantService assistantService,
         ConversationService conversationService)
     {
+        _ragService = ragService;
         _assistantService = assistantService;
         _conversationService = conversationService;
     }
@@ -23,10 +26,15 @@ public class ChatController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<ChatResponse>> Chat(string message)
     {
-        var answer =
+        var ragAnswer = await _ragService.AskAsync(message);
+
+        if (ragAnswer != null)
+            return Ok(ragAnswer);
+
+        var toolAgentAswer =
             await _assistantService.AskAsync(message);
 
-        return Ok(answer);
+        return Ok(toolAgentAswer);
     }
 
     [HttpDelete("history")]
